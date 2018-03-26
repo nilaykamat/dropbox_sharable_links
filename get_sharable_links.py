92ebd0a06812bde037d2dd97381c9d8b05f55116
nilay <nilay@browntape.com>
2018-03-26 12:36:47 +0530

    Added support fordatabase and token tracking

--- get_sharable_links.py
+++ get_sharable_links.py
@@ -1,9 +1,12 @@
 import dropbox
 import csv
 import os
+import json
+from time import gmtime, strftime
 import random
 import string
 from threading import Thread
+from tinydb import TinyDB, Query
 
 def create_links(foldername, csvfile) :
     filesList = []
@@ -31,20 +34,65 @@ def create_links(foldername, csvfile) :
         print "Exception Happened"
         print foldername
 
+
+def thread_lifecycle(foldername, csvfile, token) :
+	# pre execution code
+	# Link creation execution
+    create_links(foldername, csvfile)
+    update_end_time(foldername, token)
+
+
 def start(api_key, folders) :
     global dbx
     global filesList
+
     dbx = dropbox.Dropbox(api_key) #dropbox api secret
 
-    directory = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+    directory = generate_random_token()
     os.makedirs('Files/'+ directory)
-
+    
     for foldername in folders :
-        filesList = []
-        nameOfFile = foldername.split('/')
-        csvfile = open('Files/' + directory + '/' + nameOfFile[-1] + '.csv',"a+")
-        os.chmod(csvfile.name, 0777)
-        t = Thread(target=create_links, args=(foldername, csvfile))
+        nameOfFile = get_file_name(foldername)
+        
+        filePath = 'Files/' + directory + '/' + nameOfFile + '.csv'
+        csvfile = create_csv(filePath)
+        
+        listElem = {
+			'token' : directory,
+            'file_path' : filePath,
+            'folder_name' : foldername,
+            'start_time' : strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+            'end_time' : False
+        }
+    	save_collection_to_db(listElem)
+
+        t = Thread(target=thread_lifecycle, args=(foldername, csvfile, directory))
         t.start()
     
     return {"status":True, "message":"Successfully queued for generating links", "token":directory}
+
+def save_collection_to_db(collection) :
+    db = TinyDB('Database/db.json')
+    db.insert(collection)
+
+def update_end_time(foldername, token) :
+    db = TinyDB('Database/db.json')
+    Token = Query()
+    db.update({'end_time': strftime("%Y-%m-%d %H:%M:%S", gmtime())}, (Token.token==token) & (Token.folder_name==foldername))
+
+def get_token_status(token) :
+    db = TinyDB('Database/db.json')
+    Token = Query()
+    return db.search(Token.token==token)
+
+def get_file_name(foldername) :
+	nameOfFile = foldername.split('/')
+	return nameOfFile[-1]
+
+def create_csv(filePath) :
+	csvfile = open(filePath, "a+")
+	os.chmod(csvfile.name, 0777)
+	return csvfile
+
+def generate_random_token() :
+	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
--- pi.py
+++ pi.py
@@ -1,6 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import get_sharable_links
-import json
 
 app = Flask(__name__)
 
@@ -8,7 +7,13 @@ app = Flask(__name__)
 def create_links():
     payload = request.get_json()
     resp = get_sharable_links.start(payload['api_key'], payload['folders'])
-    return json.dumps(resp)
+    return jsonify(resp)
+
+@app.route("/get_status/<token>")
+def get_status(token):
+	tokenResponse = get_sharable_links.get_token_status(token)
+	return jsonify(tokenResponse)
+
 
 if (__name__ == "__main__"):
     app.run(port = 5000)
