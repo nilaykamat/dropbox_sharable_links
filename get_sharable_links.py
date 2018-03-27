e78d20c27719f9bfcdf7a0fb23557635685446b5
nilay <nilay@browntape.com>
2018-03-27 12:21:17 +0530

    Uncommented important code

--- get_sharable_links.py
+++ get_sharable_links.py
@@ -23,9 +23,9 @@ def create_links(foldername, csvfile) :
 
         for file in filesList :
             if (isinstance(file, dropbox.files.FileMetadata)) :
-                filename = file.name + ',' + file.path_lower + '\n'
-                # link_data = dbx.sharing_create_shared_link(file.path_lower)
-                # filename += link_data.url + '\n'
+                filename = file.name + ',' + file.path_lower + ','
+                link_data = dbx.sharing_create_shared_link(file.path_lower)
+                filename += link_data.url + '\n'
                 csvfile.write(filename)
                 print(file.name)
             else :
